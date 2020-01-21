5ecb90a36de678cd7b6e3bf58b7eacd43eaa1ff8
Brett Walton <bmw0045@auburn.edu>
2020-01-21 00:34:18 -0600

    background

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
+
+//init app
 const app = express();
 
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static('src'));
+
+//home route
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.render('index')
 });
 
 app.listen(3000, function(){
