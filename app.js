43f74b58b42ef3a81ce2b6e7e92a2d45115d4ffa
Brett Walton <bmw0045@auburn.edu>
2020-01-20 23:27:07 -0600

    hello world for localhost

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World')
+});
+
+app.listen(3000, function(){
+    console.log('////////// W E L C O M E //////////')
+    console.log('PickEm running on localhost:3000...')
+    console.log('///////////////////////////////////')
+});
